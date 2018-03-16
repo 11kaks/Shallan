cda6491f1b2d046c5626cfe78c65ff4d09e2f1e4
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2018-03-16 21:04:11 +0200

    Main ready for Glew

--- FreeGLUTproj/main.cpp
+++ FreeGLUTproj/main.cpp
@@ -1,5 +1,7 @@
 
+#include <iostream>
 
+#include <glew.h>
 #include <freeglut.h>
 
 void display() {
@@ -14,6 +16,8 @@ void display() {
 
 int main(int argc, char* argv[]) {
 
+	std::cout << "Starting" << std::endl;
+
 	glutInit(&argc, argv);
 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
@@ -21,6 +25,18 @@ int main(int argc, char* argv[]) {
 	glutInitWindowSize(320, 320);   // Set the window's initial width & height
 	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
 	glutDisplayFunc(display); // Register display callback handler for window re-paint
+
+	GLenum err = glewInit();
+
+	if(GLEW_OK != err) {
+		/* Problem: glewInit failed, something is seriously wrong. */
+		//fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+		std::cout << "Glew error " << std::endl;
+	} else {
+		std::cout << "Glew working " << std::endl;
+	}
+
+
 	glutMainLoop();           // Enter the infinitely event-processing loop
 
 	return 0;
