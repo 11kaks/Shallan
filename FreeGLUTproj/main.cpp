748c3ac5c2a864426fa20d2f0a2c5323d5022fba
Riihiaho <kiauriih@jyu.fi>
2018-03-21 13:52:41 +0200

    Ohjauksessa puuhailtua

--- FreeGLUTproj/main.cpp
+++ FreeGLUTproj/main.cpp
@@ -36,8 +36,6 @@ Called when resizing the window.
 */
 void reshape(int w, int h) {
 
-	cout << "reshape" << endl;
-
 	// Prevent a divide by zero, when window is too short
 	// (you cant make a window of zero width).
 	if(h == 0)
@@ -68,9 +66,28 @@ void display(void) {
 	glLoadIdentity();
 
 	glTranslatef(0.0f, 0.0f, -5.0f);
+	glRotatef(40.0f, 1.0f, 0.5f, 0.5f);
 	glutSolidCube(1.0f);
 
 	glutSwapBuffers();
+}
+
+
+void init(void)
+{
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
 }
 
 int main(int argc, char* argv[]) {
@@ -85,7 +102,7 @@ int main(int argc, char* argv[]) {
 	glutInitWindowPosition(k_windowPosX, k_windowPosY);
 	glutInitWindowSize(k_windowSizeX, k_windowSizeY);
 	glutCreateWindow("Freeglut test");
-
+	init();
 	// register callbacks
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
@@ -105,7 +122,27 @@ int main(int argc, char* argv[]) {
 		std::cout << "Glew error " << std::endl;
 	} else {
 		std::cout << "Glew working " << std::endl;
+
+		if (glewIsSupported("GL_VERSION_1_4  GL_ARB_point_sprite"))
+		{
+			std::cout << "Great, we have OpenGL 1.4 + point sprites." << std::endl;
+		}
+
+		if (!glewGetExtension("GL_ARB_shading_language_100")||
+			!glewGetExtension("GL_ARB_vertex_shader")||
+			!glewGetExtension("GL_ARB_fragment_shader")||
+			!glewGetExtension("GL_ARB_geometry_shader")||
+			!glewGetExtension("GL_ARB_geometry_shader4")||
+			!glewGetExtension("GL_ARB_shader_objects")) {
+		std:cout << "Kaikkii kivoi extensioneita löäytyy" << std::endl;
+		}
+		else {
+		std::cout << "Jotain extensioneita puuttuu" << std::endl;
+		}
+
 	}
+
+
 
 	// Enter the infinitely event-processing loop
 	glutMainLoop();
