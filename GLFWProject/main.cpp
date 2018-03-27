77cec3ebdf0b2d56db79b2014136de004bc9d3ff
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2018-03-27 12:12:47 +0300

    Transformating triangle with vertex shader

--- GLFWProject/main.cpp
+++ GLFWProject/main.cpp
@@ -126,7 +126,32 @@ int main(int argc, char** argv) {
 	GLuint vertexbuffer = initVertexBuffer();
 	
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders("shaders/SimpleVertexShader.vertexshader", "shaders/SimpleFragmentShader.fragmentshader");
+	GLuint programID = LoadShaders("shaders/SimpleTransform.vertexshader", "shaders/SimpleFragmentShader.fragmentshader");
+
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+	// Camera matrix
+	glm::mat4 View = glm::lookAt(
+		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+		glm::vec3(0, 0, 0), // and looks at the origin
+		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);
+
+	// Testing some transformations
+	/*Model = glm::rotate(Model, 30.0f, glm::vec3(0., 1., 0.));
+	Model = glm::translate(Model, glm::vec3(0., 0., 4.));
+	Model = glm::scale(Model, glm::vec3(3.));*/
+
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
 	while(!glfwWindowShouldClose(window)) {
 		// Setup view
@@ -134,6 +159,10 @@ int main(int argc, char** argv) {
 
 		// Use our shader
 		glUseProgram(programID);
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 		
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
