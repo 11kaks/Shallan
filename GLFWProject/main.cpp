d79515c9c56e9322d1e7fb9e3b655a38a3719a3d
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2018-03-27 15:42:40 +0300

    Simple object loading

--- GLFWProject/main.cpp
+++ GLFWProject/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <glm/gtc/matrix_transform.hpp> // translate, rotate..
 
@@ -11,8 +12,13 @@
 
 
 #include "shader.hpp"
+#include "objloader.hpp"
 
 using namespace std;
+
+const char* objFilePath = "objects/cube.obj";
+const char* vertexShaderPath = "shaders/SimpleVertexShader.vertexshader";
+const char* fragmentShaderPath = "shaders/SimpleFragmentShader.fragmentshader";
 
 void errorCallback(int error, const char* description) {
 	cerr << "[ERROR] - " << description << endl;
@@ -95,47 +101,11 @@ void initDisplay() {
 
 GLuint initVertexBuffer() {
 
-
-	// Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat g_vertex_buffer_data[] = {
-		-1.0f,-1.0f,-1.0f, // triangle 1 : begin
-		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f, // triangle 1 : end
-		1.0f, 1.0f,-1.0f, // triangle 2 : begin
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f, // triangle 2 : end
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f,-1.0f,
-		1.0f,-1.0f,-1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		1.0f,-1.0f, 1.0f
-	};
+	// Read our .obj file
+	std::vector< glm::vec3 > vertices;
+	std::vector< glm::vec2 > uvs;
+	std::vector< glm::vec3 > normals; // Won't be used at the moment.
+	bool res = loadOBJ(objFilePath, vertices, uvs, normals);
 
 	// This will identify our vertex buffer
 	GLuint vertexbuffer;
@@ -144,7 +114,7 @@ GLuint initVertexBuffer() {
 	// The following commands will talk about our 'vertexbuffer' buffer
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	// Give our vertices to OpenGL.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
 	return vertexbuffer;
 }
 
@@ -214,7 +184,7 @@ int main(int argc, char** argv) {
 	GLuint colorbuffer = initColorBuffer();
 	
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders("shaders/SimpleVertexShader.vertexshader", "shaders/SimpleFragmentShader.fragmentshader");
+	GLuint programID = LoadShaders(vertexShaderPath, fragmentShaderPath);
 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
