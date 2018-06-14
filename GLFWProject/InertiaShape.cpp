f1323516d4b4a39f67f9f62fe3b27eb99a61f0e6
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2018-06-14 10:26:18 +0300

    Commented InertiaShape

--- GLFWProject/InertiaShape.cpp
+++
@@ -1,1 +0,0 @@
-#include "InertiaShape.h"
--- GLFWProject/InertiaShape.h
+++ GLFWProject/InertiaShape.h
@@ -1,7 +1,20 @@
 #pragma once
 
 #include <glm/glm.hpp>
+/*
+@author Kimmo Riihiaho (kimmo.riihiaho@gmail.com)
+@date 14.6.2018
 
+InertiaShape describes how the object should react to 
+rotation. Hollow ball would be more difficult to get
+rolling than a solid ball of same mass.
+
+InertiaShape offers inertia as a 3 x 3 matrix that 
+can be used as scaling factor when computing angular
+momemtum and angular velocity. Inertia tensor remains
+constant through simulation though it does depend on 
+objects mass.
+*/
 class InertiaShape
 {
 public:
@@ -20,16 +33,22 @@ protected:
 };
 
 /*
-Inertia of a origosymmetric box.
+Inertia of a origosymmetric solid box.
 */
 class InertiaBox : public InertiaShape
 {
 public:
-	InertiaBox(glm::vec3 corner) {
 
-		float x = corner[0] / 2.0f;
-		float y = corner[1] / 2.0f;
-		float z = corner[2] / 2.0f;
+	/*
+	Construct new box inertia by providing
+	one corner point of the box.
+	*/
+	InertiaBox(glm::vec3 corner) {
+		// Absolute taken to define the box 
+		// in the positive octant of the space.
+		float x = abs(corner[0]) / 2.0f;
+		float y = abs(corner[1]) / 2.0f;
+		float z = abs(corner[2]) / 2.0f;
 		m_inertiaTensor = glm::mat3(
 			y*y + z * z, 0.0f, 0.0f,
 			0.0f, x*x + z * z, 0.0f,
@@ -38,9 +57,15 @@ public:
 	}
 	~InertiaBox() {}
 
+	/*
+	Get the inertia tensor for object with given mass.
+	*/
 	glm::mat3 getInertiaTensor(float mass) {
 		return (mass / 12.0f) * m_inertiaTensor;
 	}
+	/*
+	Get the inverse of inertia tensor for object with given mass.
+	*/
 	glm::mat3 getInertiaTensorInv(float mass) {
 		return (mass / 12.0f) * glm::inverse(m_inertiaTensor);
 	}
