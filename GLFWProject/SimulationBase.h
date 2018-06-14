6f9e6c35fce4a67cb14eeb23a1a71105309f53d6
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2018-06-14 10:42:34 +0300

    SimulationBase commented.

--- GLFWProject/SimulationBase.h
+++ GLFWProject/SimulationBase.h
@@ -1,12 +1,38 @@
 #pragma once
 
 #include <vector>
+/*
+@author Kimmo Riihiaho, kimmo.riihiaho@gmail.com
+@date 14.6.2018
 
+SimulationBase interface offers the few methods our Ode
+solver requires.
+*/
 class SimulationBase {
 public:
-	virtual void getState(std::vector<float> &dst) = 0;
-	virtual void setState(std::vector<float> &src) = 0;
-	virtual void derivate(std::vector<float> &dst) = 0;
-	virtual std::size_t getDim() = 0;
+	/*
+	Get state of all the objects in simulation as a flat list.
+	The state is set to dst vector.
 
+	@param dst must be allocated to proper legth (use getDim())
+			   before using.
+	*/
+	virtual void getState(std::vector<float> &dst) = 0;
+	/*
+	Set state of all the objects in simulation from a flat list.
+	*/
+	virtual void setState(std::vector<float> &src) = 0;
+	/*
+	Apply forces and torque, then return the time derivative
+	of the system in vector dst.
+
+	@param dst must be allocated to proper legth (use getDim())
+			   before using.
+	*/
+	virtual void derivate(std::vector<float> &dst) = 0;
+	/*
+	Returns length of the list needed to store the state
+	of all objects in the system.
+	*/
+	virtual std::size_t getDim() = 0;
 };
